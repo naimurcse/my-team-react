44ad925ce1b5dd62906c20a9e3315971fec7ba5a
Naimur Rahaman <2015naim@gmail.com>
2021-08-04 23:01:24 +0600

    draf component done

--- src/components/Draf/Draf.js
+++ src/components/Draf/Draf.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./Draf.css"
 
-const Draf = () => {
+const Draf = (props) => {
+
+    const {name,img,playingRole,salary} = props.playerInfo;
+    console.log(name);
     return (
         <div className="draf-container">
-            <h2>Draf List</h2>
+            <div className="img-container">
+                <img src={img} alt=""></img>
+            </div>
+            <div className="player-title">
+                <h2>{name}</h2>
+                <p>{playingRole}</p>
+            </div>
+
+            <div style={{textAlign: 'center'}}>
+                <p>Salary</p>
+                <h2>{salary}</h2>
+            </div>
+            <div style={{paddingRight:'0'}}>
+                <button className="btn">Add the player</button>
+            </div>
         </div>
     );
 };
--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import Cart from '../Cart/Cart';
 import Draf from '../Draf/Draf';
-import "./Players.css"
+import playerData from "../../playersData/playersData.json";
+import "./Players.css";
 
 const Players = () => {
+
+    console.log(playerData);
+    const [playerInfo,setPlayerInfo] = useState([]);
+
+    useEffect(() =>{
+        setPlayerInfo(playerData);
+        // console.log(playerInfo);
+    },[]);
+
     return (
-        <div>
-            <Draf></Draf>
-            <Cart></Cart>
+        <div className="players-container">
+            <div className="draf">
+                {
+                    playerInfo.map((listOfPlayers) => <Draf playerInfo={listOfPlayers}></Draf>)
+                }
+            </div>
+            <div className="cart">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
